94d5595e4b4e482dbab230f7b13435b825653b19
Fady Mohsen <fadymohsen62@gmail.com>
2024-08-23 04:01:30 +0300

    commit checker

--- routers/admin.js
+++ routers/admin.js
@@ -3,22 +3,16 @@ const router = express.Router();
 const adminController = require('../controllers/admin');
 const verfiyAdminToken = require('../middleware/verfiyToken');
 
-router.route('/add')
+router.route('/')
     .post(verfiyAdminToken, adminController.addAdmin)
-
-router.route('/login')
-    .post(adminController.adminLogin)
-
-router.route('/admins')
     .get(verfiyAdminToken, adminController.getAdmins)
-
-router.route('/updateadmin')
     .patch(verfiyAdminToken, adminController.adminUpdateData)
-
-router.route('/delete')
     .delete(verfiyAdminToken, adminController.deleteAdmin)
 
-router.route('/')
+router.route('/adminlogin')
     .post(adminController.adminLogin)
+
+router.route('/checker')
+    .get(verfiyAdminToken, adminController.checker)
 
 module.exports = router;
